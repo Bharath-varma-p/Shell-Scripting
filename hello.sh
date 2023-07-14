74b83d30eac7049da05f41d2f64e52be1b561466
bharath <bharath@bharaths-MacBook-Pro.local>
2023-07-14 19:33:21 -0400

    Shell Script

---
+++ hello.sh
@@ -0,0 +1,23 @@
+echo "Hello, Bharath How are you is everything ok"
+
+
+
+# 1. Creating a Variable and User Inputs
+
+## Write a script that prompts the user for their name and greets 
+## them with a personalized message.
+
+echo "Enter your name:"
+read name
+echo "Hello, $name! Welcome to the world of shell scripting."
+echo "Setup an Temperature"
+read degree
+echo "The weather is \"$degree\" today."
+
+
+
+echo "End of the Terminal"
+
+
+
+
