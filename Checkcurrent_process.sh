34308614835573b4a97f06aa127fc366661677ac
Penumatcha Bharath Varma <50948937+Bharath-varma-p@users.noreply.github.com>
2023-07-16 13:55:43 -0400

    code for process status

---
+++ Checkcurrent_process.sh
@@ -0,0 +1,25 @@
+<<command
+    Write a shell script that prompts the user for a process name and checks 
+    if the process is currently running. 
+    If it is running, display its process ID (PID). If it is not running,
+     start the process.
+command
+
+
+
+#!/bin/bash
+
+echo "Please enter the process name I will check it for you if it is runnig or not:"
+read -r process_name
+
+# Check if the process is running
+if pgrep "$process_name" >/dev/null; then
+    echo "$process_name is already running with PID $(pgrep "$process_name")."
+else
+    echo "$process_name is not currently running. Starting $process_name..."
+    
+    # Start the process
+    "$process_name" &
+    
+    echo "$process_name started with PID $!"
+fi
